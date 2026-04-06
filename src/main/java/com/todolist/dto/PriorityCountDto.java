4b9e8cbe890e685289e7a4c7679d4b797601d575
loopydoopy <loopydoopy@MacBook-Pro-loopydoopy.local>
2026-04-06 11:29:26 +0300

    add PriorityCountDto

---
+++ src/main/java/com/todolist/dto/PriorityCountDto.java
@@ -0,0 +1,14 @@
+package com.todolist.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriorityCountDto {
+  private String priority;
+  private long count;
+}
+
